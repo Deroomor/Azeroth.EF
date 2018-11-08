1a4f8fd5e5a03e93fb4802df0f4882648bc25c1f
eeroom <eedroom@163.com>
2018-11-08 22:56:16 +0800

    2018年11月8日

--- EF.T4.Mysql/Program.cs
+++ EF.T4.Mysql/Program.cs
@@ -10,23 +10,6 @@ namespace EF.T4.Mysql
     {
         static void Main(string[] args)
         {
-            //DbContext dbcontext = new DbContext();
-
-            //Company_Shares cs = new Company_Shares() {  Id=Guid.NewGuid().ToString().Substring(0,32),
-            // Tenant_Id= Guid.NewGuid().ToString().Substring(0, 32)
-            // , Total_Shares=200};
-
-            //dbcontext.Entry(cs).State = System.Data.Entity.EntityState.Added;
-            //dbcontext.SaveChanges();
-
-            //var lst= dbcontext.Company_Shares.ToList();
-
-            //var lst2= dbcontext.Plan.Where(x=>x.Id=="sss").ToList();
-
-       
-            //System.Data.DataTable table = new System.Data.DataTable();
-            //string inheritFlag = "sssssssssss";
-            //bool isInherit= table.Columns.OfType<System.Data.DataColumn>().Any(x => x.ColumnName.ToLower() == inheritFlag);
         }
     }
 }
