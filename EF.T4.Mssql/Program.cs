f3f8b89cfa99336ae9d101076a487f993d31d062
Deroomor <eedroom@163.com>
2019-03-01 09:52:02 +0800

    System.Threading.Interlocked使用

--- EF.T4.Mssql/Program.cs
+++ EF.T4.Mssql/Program.cs
@@ -6,69 +6,125 @@ using System.Threading.Tasks;
 
 namespace EF.T4.Mssql
 {
-    class Program
+    class Student
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public int ClassId { get; set; }
+    }
+
+    class MyClass
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+
+    unsafe class Program
+    {
+        static int processing = 0;
         static void Main(string[] args)
         {
-            //string cnnstr = System.Configuration.ConfigurationManager.ConnectionStrings["mssqlmaster"].ConnectionString;
-            //using (var cnn = new System.Data.SqlClient.SqlConnection(cnnstr))
+            try
+            {
+                int* vptr = null;
+                *vptr = 100;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+
+
+            var lstStudent = System.Linq.Enumerable.Range(1, 10).Select(x => new Student() { Age = x, ClassId = x % 4, Id = x, Name = "同学" + x }).ToList();
+
+            var lstclass = System.Linq.Enumerable.Range(1, 5).Select(x => new MyClass() { Id = x, Name = "班级" + x }).ToList();
+
+            var dict = lstclass.GroupJoin(lstStudent, x => x.Id, y => y.ClassId, (x, y) => new { x, y })
+                .ToDictionary(x => x.x, x => x.y.ToList());
+
+            while (true)
+            {
+                var str = System.Console.ReadLine();
+                if (System.Threading.Interlocked.Exchange(ref processing, 1) != 0)
+                {
+                    Console.WriteLine("系统繁忙，请稍后再尝试");
+                    continue;
+                }
+                System.Threading.ThreadPool.QueueUserWorkItem(x => {
+                    Console.WriteLine("处理中....");
+                    System.Threading.Thread.Sleep(5 * 1000);
+                    Console.WriteLine($"你输入的内容是：{str}");
+                    System.Threading.Interlocked.Exchange(ref processing, 0);
+                });
+
+
+            }
+
+
+
+            //var lst = System.Linq.Enumerable.Range(1, 1 - 1).Select(x => new DateTime(2019, x, 1).ToString()).ToList();
+
+
+            //double i = 123456789012345.256487987588888888;
+            ////1234567890123456.7
+            ////1125899906842624.3
+            //while (true)
             //{
-            //    List<string> lstTableName = new List<string>();
-            //    using (var cmd = cnn.CreateCommand())
-            //    {
-            //        cmd.CommandText = "select TABLE_NAME from INFORMATION_SCHEMA.TABLES where TABLE_TYPE='BASE TABLE' ORDER BY TABLE_NAME";
-            //        using (var reader = cmd.ExecuteReader())
-            //        {
-            //            while (reader.Read())
-            //            {
-            //                lstTableName.Add(reader.GetString(0));
-            //            }
-            //        }
-            //    }
-            //    foreach (string nameClass in lstTableName)
-            //    {
-            //        //manager.StartNewFile(nameClass + ".cs");
-            //        //this.WriteLine(string.Format("{1}public partial class {0}", nameClass, new String(' ', 4)));
-            //        //this.WriteLine(new String(' ', 4) + "{");
-            //        System.Data.DataTable schemaTable;
-            //        using (var cmd = cnn.CreateCommand())
-            //        {
-            //            cmd.CommandText = "select * from " + nameClass;
-            //            using (var reader = cmd.ExecuteReader(System.Data.CommandBehavior.KeyInfo | System.Data.CommandBehavior.SchemaOnly))
-            //            {
-            //                schemaTable = reader.GetSchemaTable();
-            //            }
-            //        }
-            //        System.Data.DataTable commentTable=new System.Data.DataTable("rt");
-            //        using (var cmd = cnn.CreateCommand())
-            //        {
-            //            cmd.CommandText = string.Format("with htt as({0}),gmm as ({1}) select htt.name,gmm.value from htt left join gmm on htt.column_id=gmm.minor_id",
-            //                 "select * from sys.columns where object_id=OBJECT_ID('" + nameClass + "')",
-            //                 "select * from sys.extended_properties where sys.extended_properties.major_id=OBJECT_ID('" + nameClass + "')");
-            //            using (var adapter=new System.Data.SqlClient.SqlDataAdapter(cmd))
-            //            {
-            //                adapter.Fill(commentTable);
-            //            }
-            //        }
-            //        foreach (System.Data.DataRow row in schemaTable.Select())
-            //        {
-            //            string colName = row["ColumnName"].ToString();
-            //            int ss = Convert.ToInt32(row["ColumnSize"]);
-            //            string colType = row["DataType"].ToString().Substring("System.".Length);
-            //            bool allowDBNull = Convert.ToBoolean(row["AllowDBNull"]);
-            //            if (allowDBNull && !colType.Equals("String") && !colType.Equals("Byte[]"))
-            //                colType = "Nullable<" + colType + ">";
-            //            if (colName.EndsWith("Enum"))
-            //                colType = colName.Substring(0, colName.Length - "Enum".Length) + "Index";
-            //            string comment = commentTable.Select("name='" + colName + "'")[0]["value"].ToString().Replace("\r\n", " ").Replace("\n", " ").Replace("\n", " ");
-            //            this.WriteLine(string.Format("{0}/// <summary>",new String(' ', 8)));
-            //            this.WriteLine(string.Format("{0}///{1}", new String(' ', 8), comment));
-            //            this.WriteLine(string.Format("{0}/// </summary>", new String(' ', 8)));
-            //            this.WriteLine(string.Format("{0}public {0} {1} {2}", new String(' ', 8), colType, colName, "{set;get;}"));
-            //        }
-            //        //manager.EndBlock();
-            //    }
+            //    if (i.ToString().ToUpper().Contains("E"))
+            //        break;
+            //    i=i*2.0;
             //}
+
+            //double mr = 87.33333333333333333333333333333333333333333333;
+
+
+            //DbContext dbcontext = new DbContext();
+            //Table_1 ta = new Table_1() {
+            //    Id = Guid.NewGuid(),
+            //    TheDecimal12 = 123456.789123m,
+            //    TheDecimal38 = 123456.789123m,
+            //    TheDecimal4 = 56.789123m,
+            //    TheFloat = mr,
+            //    TheReal = 123456.789123f,
+            //};
+
+            ////double tmp =12345678901234567890123456789.1234567890123456789;
+            //            //79228162514264337593543950335
+            //decimal tmp = 9999999999999999.123456789012m;
+            ////decimal.MaxValue
+            //Table_1 maxTa = new Table_1() {
+            //    Id = Guid.NewGuid(),
+            //    //TheDecimal12 = (decimal)tmp,
+            //    TheDecimal38 = tmp,
+            //    //TheDecimal4 = (decimal)tmp,
+            //    TheFloat = (double)tmp,
+            //    TheReal = (float)tmp,
+            //};
+
+            ////double m = 1.25E+10;
+
+            //decimal mintmp = 0.00000000000000000000123456m;
+            //Table_1 minTa = new Table_1() {
+            //     Id=Guid.NewGuid(),
+            //      TheDecimal12=mintmp,
+            //      TheDecimal38=mintmp,
+            //       TheDecimal4=mintmp,
+            //        TheFloat=(double)mintmp,
+            //         TheReal=(float)mintmp
+            //};
+
+            //dbcontext.Table_1.Add(ta);
+
+            //dbcontext.Table_1.Add(maxTa);
+            //dbcontext.Table_1.Add(minTa);
+            //var rt= dbcontext.SaveChanges();
         }
     }
-}
